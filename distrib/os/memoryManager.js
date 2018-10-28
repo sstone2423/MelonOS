2c10ce7fc16c4a499f9f9997da3d8489ac07e91e
Stephen Stone <sstone2423@gmail.com>
2018-10-27 23:06:23 -0400

    Fixed the memory display on loadwq

--- distrib/os/memoryManager.js
+++ distrib/os/memoryManager.js
@@ -32,52 +32,7 @@ var TSOS;
                     // Add pcb to waitingQueue
                     this.waitingQueue.enqueue(pcb);
                     // Update the pcb info to the tableProcess
-                    // Initialize table variable
-                    var tableProcesses = document.getElementById("tableProcesses");
-                    // Create a new row for the new process
-                    var newRow = document.createElement("TR");
-                    newRow.setAttribute("id", "processTR");
-                    tableProcesses.appendChild(newRow);
-                    // Create a column for PID
-                    var colPID = document.createElement("TD");
-                    colPID.setAttribute("id", "colPID");
-                    newRow.appendChild(colPID);
-                    colPID.innerHTML = pcb.pId.toString();
-                    // Create a column for PC
-                    var colPC = document.createElement("TD");
-                    colPC.setAttribute("id", "colPC");
-                    newRow.appendChild(colPC);
-                    colPC.innerHTML = pcb.PC.toString();
-                    // Create a column for ACC
-                    var colACC = document.createElement("TD");
-                    colACC.setAttribute("id", "colACC");
-                    newRow.appendChild(colACC);
-                    colACC.innerHTML = pcb.Acc.toString();
-                    // Create a column for X
-                    var colX = document.createElement("TD");
-                    colX.setAttribute("id", "colX");
-                    newRow.appendChild(colX);
-                    colX.innerHTML = pcb.xReg.toString();
-                    // Create a column for Y
-                    var colY = document.createElement("TD");
-                    colY.setAttribute("id", "colY");
-                    newRow.appendChild(colY);
-                    colY.innerHTML = pcb.yReg.toString();
-                    // Create a column for Z flag
-                    var colZ = document.createElement("TD");
-                    colZ.setAttribute("id", "colZ");
-                    newRow.appendChild(colZ);
-                    colZ.innerHTML = pcb.zFlag.toString();
-                    // Create a column for Priority
-                    var colPriority = document.createElement("TD");
-                    colPriority.setAttribute("id", "colPriority");
-                    newRow.appendChild(colPriority);
-                    colPriority.innerHTML = pcb.priority.toString();
-                    // Create a column for State
-                    var colState = document.createElement("TD");
-                    colState.setAttribute("id", "colState");
-                    newRow.appendChild(colState);
-                    colState.innerHTML = pcb.state;
+                    TSOS.Control.hostMemory();
                 }
             }
         };
--- source/os/memoryManager.ts
+++ source/os/memoryManager.ts
@@ -39,52 +39,7 @@
                     // Add pcb to waitingQueue
                     this.waitingQueue.enqueue(pcb);
                     // Update the pcb info to the tableProcess
-                    // Initialize table variable
-                    let tableProcesses = document.getElementById("tableProcesses");
-                    // Create a new row for the new process
-                    let newRow = document.createElement("TR");
-                    newRow.setAttribute("id", "processTR");
-                    tableProcesses.appendChild(newRow);
-                    // Create a column for PID
-                    let colPID = document.createElement("TD");
-                    colPID.setAttribute("id", "colPID");
-                    newRow.appendChild(colPID);
-                    colPID.innerHTML = pcb.pId.toString();
-                    // Create a column for PC
-                    let colPC = document.createElement("TD");
-                    colPC.setAttribute("id", "colPC");
-                    newRow.appendChild(colPC);
-                    colPC.innerHTML = pcb.PC.toString();
-                    // Create a column for ACC
-                    let colACC = document.createElement("TD");
-                    colACC.setAttribute("id", "colACC");
-                    newRow.appendChild(colACC);
-                    colACC.innerHTML = pcb.Acc.toString();
-                    // Create a column for X
-                    let colX = document.createElement("TD");
-                    colX.setAttribute("id", "colX");
-                    newRow.appendChild(colX);
-                    colX.innerHTML = pcb.xReg.toString();
-                    // Create a column for Y
-                    let colY = document.createElement("TD");
-                    colY.setAttribute("id", "colY");
-                    newRow.appendChild(colY);
-                    colY.innerHTML = pcb.yReg.toString();
-                    // Create a column for Z flag
-                    let colZ = document.createElement("TD");
-                    colZ.setAttribute("id", "colZ");
-                    newRow.appendChild(colZ);
-                    colZ.innerHTML = pcb.zFlag.toString();
-                    // Create a column for Priority
-                    let colPriority = document.createElement("TD");
-                    colPriority.setAttribute("id", "colPriority");
-                    newRow.appendChild(colPriority);
-                    colPriority.innerHTML = pcb.priority.toString();
-                    // Create a column for State
-                    let colState = document.createElement("TD");
-                    colState.setAttribute("id", "colState");
-                    newRow.appendChild(colState);
-                    colState.innerHTML = pcb.state;
+                    Control.hostMemory();
                 }
             }
         }
